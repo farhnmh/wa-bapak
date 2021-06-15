0e03ad600d9e9b3e60bdca86aa18f5844b8a74bf
arlomd <arlomariod@gmail.com>
2021-06-15 22:12:08 +0700

    Car Movement Mechanic

---
+++ WA Bapak/Assets/Script/CameraStable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraStable : MonoBehaviour
+{
+    public GameObject theCar;
+    // Start is called before the first frame update
+
+    public float carX, carY, carZ;
+    // Update is called once per frame
+    void Update()
+    {
+        carX = theCar.transform.eulerAngles.x;
+        carY = theCar.transform.eulerAngles.y;
+        carZ = theCar.transform.eulerAngles.z;
+    }
+}
---
+++ WA Bapak/Assets/Script/User.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class User
+{
+    public string uname;
+    public string password;
+
+    public User(string _uname, string _pw)
+    {
+        uname = _uname;
+        password = _pw;
+    }
+
+}
+
